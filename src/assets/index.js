d3dcf164bf62fd0cf95d1821c5d3059798fa3992
jonny-grace <johnlemma9@gmail.com>
2023-12-24 01:56:59 +0300

    error debuging

--- src/assets/index.js
+++ src/assets/index.js
@@ -46,12 +46,7 @@ import dcc from './company/dcc.png';
 import kelhel from './company/kelhel.png';
 import microverse from './company/microverse.png';
 
-import komikult from './projects/komikult.png';
-import leaderboard from './projects/leaderboard.png';
-import math from './projects/math-magicians.png';
-import movie from './projects/movie-metro.png';
-import nyeusi from './projects/nyeusi.png';
-import space from './projects/space-hub.png';
+
 
 import gym from './projects/gym.jpg';
 import food from './projects/food.jpg';
@@ -100,12 +95,7 @@ export {
   redux,
   tailwind,
   typescript,
-  komikult,
-  leaderboard,
-  math,
-  movie,
-  nyeusi,
-  space,
+ 
   coverhunt,
   dcc,
   kelhel,
